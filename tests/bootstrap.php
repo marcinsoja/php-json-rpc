cbc3b4827a30d5c814de0a29027ac1e3d8cd51d8
Marcin Soja <dev@marcinsoja.pl>
2013-04-01 01:20:30 +0200

    Changing the class loader (tests).

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,4 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/lib/JsonRpcLib/autoload.php';
