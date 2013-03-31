aca5cfffb8939fcaa9d937f344ed74ef67f62f92
Marcin Soja <dev@marcinsoja.pl>
2013-03-31 03:46:35 +0200

    Added new Input/Data - Get.

---
+++ lib/JsonRpcLib/Server/Input/Data/Get.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace JsonRpcLib\Server\Input\Data;
+
+class Get implements DataInterface
+{
+    private $param = null;
+    
+    /**
+     * @param string $param
+     */
+    public function __construct($param = 'data') {
+        $this->param = $param;
+    }
+    /**
+     * @return string
+     */
+    public function read()
+    {
+        return isset($_GET[$this->param])
+            ? $_GET[$this->param]
+            : ''
+        ;
+    }
+}
