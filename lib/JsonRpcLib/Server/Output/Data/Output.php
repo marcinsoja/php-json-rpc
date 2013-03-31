a46a5eb4bd98e446b09a8e2f9393a969687fa66b
Marcin Soja <dev@marcinsoja.pl>
2013-03-31 03:44:31 +0200

    Added http header - Data/Output.

--- lib/JsonRpcLib/Server/Output/Data/Output.php
+++ lib/JsonRpcLib/Server/Output/Data/Output.php
@@ -10,6 +10,22 @@ class Output implements DataInterface
      */
     public function write($data)
     {
+        $this->sendHeaders($data);
+        
         echo $data;
+    }
+    
+    public function sendHeaders($data)
+    {
+        if (headers_sent()) {
+            return;
+        }
+
+        if (empty($data)) {
+            header('HTTP/1.1 204 No Content');
+            return;
+        }
+
+        header('Content-Type: application/json');
     }
 }
