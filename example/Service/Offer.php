a41ad2339d999e2442368b9d284be0ba04446d30
Marcin Soja <dev@marcinsoja.pl>
2013-03-31 03:47:34 +0200

    Added server example.

---
+++ example/Service/Offer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Example\Service;
+
+class Offer {
+    public function get($id) {
+        return array('id'=>$id, 'name'=>'Sample offer');
+    }
+    private function sum($a, $b) {
+        return $a + $b;
+    }
+}
---
+++ example/server.php
@@ -0,0 +1,19 @@
+<?php 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'Service/Offer.php';
+require_once '../vendor/autoload.php';
+
+$serviceOffer = new \Example\Service\Offer();
+
+$server = new \JsonRpcLib\Server\Server();
+
+$server->addService(function() {
+    return time();
+}, 'getTime');
+
+$server->addService($serviceOffer, 'Offer');
+
+// server.php?data={"jsonrpc":"2.0","method":"Offer.sum","params":[5,5],"id":4}
+$server->handle(new \JsonRpcLib\Server\Input\Message(new \JsonRpcLib\Server\Input\Data\Get('data')));
