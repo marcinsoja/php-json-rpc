698c20648cf2a2726c32b9cc583298f698182698
Marcin Soja <dev@marcinsoja.pl>
2013-05-30 21:13:50 +0200

    CS.

--- lib/JsonRpcLib/Server/Exception.php
+++ lib/JsonRpcLib/Server/Exception.php
@@ -5,20 +5,21 @@ namespace JsonRpcLib\Server;
 class Exception extends \JsonRpcLib\Exception
 {
     private $data = null;
-    
-    public function __construct($message = null, $code = 0, $previous = null, $data = null) {
-        
-        if(is_array($previous)) {
+
+    public function __construct($message = null, $code = 0, $previous = null, $data = null)
+    {
+        if (is_array($previous)) {
             $data = $previous;
             parent::__construct($message, $code);
         } else {
             parent::__construct($message, $code, $previous);
         }
-        
+
         $this->data = $data;
     }
-    
-    public function getData() {
+
+    public function getData()
+    {
         return $this->data;
     }
 }
