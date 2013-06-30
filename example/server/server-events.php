4ccb9c321d9b62f4b78303597862f179a177dea1
Marcin Soja <dev@marcinsoja.pl>
2013-06-30 14:27:44 +0200

    Organize examamples (server/client namespace directory).

--- example/server/server-events.php
+++ example/server/server-events.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once '../vendor/autoload.php';
+require_once '../../vendor/autoload.php';
 require_once './UserService.php';
 
 use JsonRpcLib\Server;
